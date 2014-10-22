ddded3f51c4e8b703a4a85c97b430ba75256de19
lonski <michal@lonski.pl>
2014-10-22 18:07:40 +0200

    corrected CmdClose filename

---
+++ src/Commands/CmdClose.h
@@ -0,0 +1,19 @@
+#ifndef CMDCLOSE_H
+#define CMDCLOSE_H
+
+#include "Command.h"
+#include "Utils/DirectionSelector.h"
+
+class CmdClose : public Command
+{
+public:
+  CmdClose();
+  virtual bool accept(TCOD_key_t &key);
+  virtual void execute(Engine* engine, Actor* executor);
+
+private:
+  DirectionSelector _dSelector;
+
+};
+
+#endif // CMDCLOSE_H
