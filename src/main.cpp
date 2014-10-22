1ef1efe49a0cb3d48538144ac51373893329ed5d
lonski <michal@lonski.pl>
2014-10-22 20:54:39 +0200

    exceptions handling in main function

--- src/main.cpp
+++ src/main.cpp
@@ -8,24 +8,34 @@ using namespace std;
 
 int main()
 {
-
-  TCODConsole::root->setCustomFont("../amarlon/terminal.png");
-  TCODConsole::initRoot(gloScreenWidth,gloScreenHeight,"Amarlon",false, TCOD_RENDERER_SDL);
-
-  TCODMouse::showCursor(false);
-  TCOD_key_t lastKey;
-
-  Engine engine;
-  engine.init();
-
-  engine.getGui()->message(":: Welcome to Amarlon! ::", TCODColor::sky);
-
-  while ( !TCODConsole::isWindowClosed() )
+  try
   {
-    engine.update();
+    TCODConsole::root->setCustomFont("../amarlon/terminal.png");
+    TCODConsole::initRoot(gloScreenWidth,gloScreenHeight,"Amarlon",false, TCOD_RENDERER_SDL);
 
-    TCODSystem::waitForEvent(TCOD_EVENT_KEY_PRESS,&lastKey,NULL, true);
-    engine.processKey(lastKey);
+    TCODMouse::showCursor(false);
+    TCOD_key_t lastKey;
+
+    Engine engine;
+    engine.init();
+
+    engine.getGui()->message(":: Welcome to Amarlon! ::", TCODColor::sky);
+
+    while ( !TCODConsole::isWindowClosed() )
+    {
+      engine.update();
+
+      TCODSystem::waitForEvent(TCOD_EVENT_KEY_PRESS,&lastKey,NULL, true);
+      engine.processKey(lastKey);
+    }
+  }
+  catch(std::exception &e)
+  {
+    std::cout << "\nError: " << e.what() << std::endl;
+  }
+  catch(...)
+  {
+    std::cout << "\nUnknown error has occured.";
   }
 
   return 0;
