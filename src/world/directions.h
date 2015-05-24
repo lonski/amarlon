eef573b68eb2fe4795477d3dd2b624a63464e8d1
lonski <michal@lonski.pl>
2015-05-24 09:25:48 +0200

    missed enum

---
+++ src/world/directions.h
@@ -0,0 +1,20 @@
+#ifndef DIRECTIONS
+#define DIRECTIONS
+
+namespace amarlon {
+
+enum class Direction
+{
+  Null  = 0,
+  West  = 1,
+  East  = 2,
+  North = 3,
+  South = 4,
+  Up    = 5,
+  Down  = 6
+};
+
+}
+
+#endif // DIRECTIONS
+
